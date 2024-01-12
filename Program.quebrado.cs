b2b9e716f62b5f4fb21add233817c4dde300109d
aslan <138014658+aslansilva7@users.noreply.github.com>
2024-01-12 10:22:33 -0300

    Add files via upload

---
+++ Program.quebrado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quebradoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;          
+            int piQuebrado = (int)pi;   
+            Console.WriteLine ("piQuebrado = " + piQuebrado);  
+
+        }
+    }
+}
